8fa324d214db17310413e7bc2a14a78b669b65d9
Da <dasong2296@gmail.com>
2021-10-26 22:36:49 -0600

    init

---
+++ pure_ocr.py
@@ -0,0 +1,102 @@
+from pytesseract import image_to_string
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageOps
+from PIL import ImageEnhance
+import timeit
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+# Start Timer
+start = timeit.default_timer()
+
+# Path to the data directory
+data_dir = Path("./CAPTCHAS_DATASET/")
+data = pd.read_csv("./CAPTCHAS_DATASET/captcha_label.csv")
+
+# Get list of all the images
+images = sorted(list(map(str, list(data_dir.glob("*.png")))))
+labels = pd.DataFrame(data, columns=['Ground_Truth'])
+characters = "0123456789ABCDEFGHJKLMNOPRSTUVWXZabcdefghijklmnopqrstuvwxyz"
+print("Number of images found: ", len(images))
+print("Number of labels found: ", len(labels))
+print("Number of unique characters: ", len(characters))
+print("Characters present: ", characters)
+
+#This function returns the result of Tesseract
+def ocr(im):
+    return image_to_string(im, lang="Captcha", config="--psm 10 -c tessedit_char_whitelist=0123456789ABCDEFGHJKLMNOPRSTUVWXZabcdefghijklmnopqrstuvwxyz")
+
+#Removing Background Color From Left to Right
+def rem_back(trg):
+    cnt = ImageEnhance.Contrast(trg)
+    trg = trg.filter(ImageFilter.MedianFilter(3))
+    trg = cnt.enhance(1)
+    mat = trg.load()
+    tol = 10
+    cn = 0
+    for r in range(0, trg.height):
+        temp = mat[0, r][0]
+        for c in range(0, trg.width):
+            if (mat[c, r][0] <= temp + tol and mat[c, r][0] >= temp - tol):
+                mat[c, r] = (255, 255)
+    return trg
+
+#Removing Background Color From Right to Left
+def rem_back_rev(trg):
+    cnt = ImageEnhance.Contrast(trg)
+    trg = trg.filter(ImageFilter.MedianFilter(3))
+    trg = cnt.enhance(1)
+    mat = trg.load()
+    tol = 10
+    cn = 0
+    for r in range(trg.height - 1, -1, -1):
+        temp = mat[trg.width - 1, r][0]
+        for c in range(trg.width - 1, -1, -1):
+            if (mat[c, r][0] <= temp + tol and mat[c, r][0] >= temp - tol):
+                mat[c, r] = (255, 255)
+    return trg
+
+
+#Making Characters ready for OCR
+def char_op(trg):
+    trg = rem_back(trg)
+    trg = rem_back_rev(trg)
+    cnt = ImageEnhance.Contrast(trg)
+    trg = cnt.enhance(2)
+    trg = trg.filter(ImageFilter.MedianFilter(9))
+    trg = trg.filter(ImageFilter.RankFilter(5,3))
+    br = ImageEnhance.Brightness(trg)
+    trg = br.enhance(1)
+    trg = trg.convert("RGB")
+    trg = ImageOps.expand(trg, 30, 'white')
+    return trg
+
+for i in images:
+    im = Image.open(i).convert("LA")
+    im = im.resize((5 * im.width, 5 * im.height), Image.ANTIALIAS)
+    im = im.filter(ImageFilter.MedianFilter(3))
+    im = im.filter(ImageFilter.GaussianBlur(3))
+    im = im.filter(ImageFilter.MedianFilter(5))
+    im = im.filter(ImageFilter.SMOOTH_MORE)
+    cnt = ImageEnhance.Contrast(im)
+    im = cnt.enhance(1)
+    a = im.crop((0, 0, 148, 150))
+    b = im.crop((148, 0, 296, 150))
+    c = im.crop((296, 0, 444, 150))
+    d = im.crop((444, 0, 592, 150))
+    e = im.crop((592, 0, 740, 150))
+
+    a = char_op(a)
+    b = char_op(b)
+    c = char_op(c)
+    d = char_op(d)
+    e = char_op(e)
+    
+    pred = "".join(ocr(a).rstrip() + ocr(b).rstrip() + ocr(c).rstrip() +
+        ocr(d).rstrip() + ocr(e).rstrip())
+    print(pred)
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)  
