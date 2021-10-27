18b31d34234e4210487f1f1655fdf47ecd78afb7
Da <dasong2296@gmail.com>
2021-10-27 01:22:23 -0600

    comment visual code

--- pure_cnn.py
+++ pure_cnn.py
@@ -94,6 +94,7 @@ train_dataset = (
     .prefetch(buffer_size=tf.data.AUTOTUNE)
 )
 
+
 validation_dataset = tf.data.Dataset.from_tensor_slices((x_valid, y_valid))
 validation_dataset = (
     validation_dataset.map(
@@ -103,17 +104,17 @@ validation_dataset = (
     .prefetch(buffer_size=tf.data.AUTOTUNE)
 )
 
-# Visualize 
-_, ax = plt.subplots(4, 4, figsize=(10, 5))
-for batch in train_dataset.take(1):
-    images = batch["image"]
-    labels = batch["label"]
-    for i in range(16):
-        img = (images[i] * 255).numpy().astype("uint8")
-        label = tf.strings.reduce_join(num_to_char(labels[i])).numpy().decode("utf-8")
-        ax[i // 4, i % 4].imshow(img[:, :, 0].T, cmap="gray")
-        ax[i // 4, i % 4].set_title(label)
-        ax[i // 4, i % 4].axis("off")
+# # Visualize 
+# _, ax = plt.subplots(4, 4, figsize=(10, 5))
+# for batch in train_dataset.take(1):
+#     images = batch["image"]
+#     labels = batch["label"]
+#     for i in range(16):
+#         img = (images[i] * 255).numpy().astype("uint8")
+#         label = tf.strings.reduce_join(num_to_char(labels[i])).numpy().decode("utf-8")
+#         ax[i // 4, i % 4].imshow(img[:, :, 0].T, cmap="gray")
+#         ax[i // 4, i % 4].set_title(label)
+#         ax[i // 4, i % 4].axis("off")
 
 # Model
 class CTCLayer(layers.Layer):
